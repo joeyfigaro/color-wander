e95ef63bf4cd29b06033ec4290ed6a648c083000
Joey Figaro <joey@joeyfigaro.com>
2022-03-18 12:49:47 -0400

    feat: many tweaks :poop:

--- config.js
+++ config.js
@@ -1,42 +1,62 @@
-var seedRandom = require('seed-random');
-var palettes = require('./lib/color-palettes.json');
-var createRandomRange = require('./lib/random-range');
+var seedRandom = require('seed-random')
+
+var palettes = require('./lib/palettes.json')
+var createRandomRange = require('./lib/random-range')
 
 module.exports = function (seed) {
   if (typeof seed === 'undefined') {
-    seed = String(Math.floor(Math.random() * 1000000));
+    seed = String(Math.floor(Math.random() * 1000000))
   }
 
-  console.log('Seed:', seed);
+  console.log('Seed:', seed)
 
-  var randomFunc = seedRandom(seed);
-  var random = createRandomRange(randomFunc);
+  var randomFunc = seedRandom(seed)
+  var random = createRandomRange(randomFunc)
 
   var maps = [
-    'sym6.jpg', 'sym3.jpg',
-    'scifi.jpg', 'nature1.jpg',
-    'map7.jpg', 'geo5.jpg', 'geo4.jpg',
-    'geo3.jpg', 'geo1.jpg', 'fractal2.jpg',
-    'fractal1.jpg', 'eye.jpg', 'city5.jpg',
-    'city2.jpg', 'church2.jpg', 'architecture.jpg',
-    'pat1.jpg'
+    // 'sym6.jpg',
+    // 'sym3.jpg',
+    // 'scifi.jpg',
+    // 'nature1.jpg',
+    // 'map7.jpg',
+    // 'geo5.jpg',
+    // 'geo4.jpg',
+    // 'geo3.jpg',
+    // 'geo1.jpg',
+    // 'fractal2.jpg',
+    // 'fractal1.jpg',
+    // 'eye.jpg',
+    // 'city5.jpg',
+    // 'city2.jpg',
+    // 'church2.jpg',
+    // 'architecture.jpg',
+    // 'pat1.jpg',
+    // 'city5.jpg',
+    // 'eye.jpg',
+    'unsplash-space-starfield.jpeg',
+    'unsplash-space-nebula.jpeg',
+    'unsplash-timessquare.jpeg',
+    'spiral-staircase-rainbow.jpeg',
+    'spiral-staircase-grayscale.jpeg',
   ].map(function (p) {
-    return 'maps/' + p;
-  });
+    return 'maps/' + p
+  })
 
-  var mapSrc = maps[Math.floor(random(maps.length))];
+  var mapSrc = maps[Math.floor(random(maps.length))]
 
   return {
     // rendering options
     random: randomFunc,
     seedName: seed,
-    pointilism: random(0, 0.1),
-    noiseScalar: [ random(0.000001, 0.000001), random(0.0002, 0.004) ],
+    // pointilism: random(0, 50.5),
+    pointilism: 0,
+    noiseScalar: [random(0.000001, 0.000001), random(0.0002, 0.008)],
     globalAlpha: 0.5,
     startArea: random(0.0, 1.5),
     maxRadius: random(5, 100),
-    lineStyle: random(1) > 0.5 ? 'round' : 'square',
-    interval: random(0.001, 0.01),
+    lineStyle: random(1) > 0.5 ? 'square' : 'round',
+    // interval: random(0.001, 0.01),
+    interval: 0.00001,
     count: Math.floor(random(50, 2000)),
     steps: Math.floor(random(100, 1000)),
     endlessBrowser: false, // Whether to endlessly step in browser
@@ -45,38 +65,40 @@ module.exports = function (seed) {
     debugLuma: false,
     backgroundScale: 1,
     backgorundFille: 'black',
+    backgroundFill: 'transparent',
+    backgroundFille: 'transparent',
     backgroundSrc: mapSrc,
 
     // browser/node options
     pixelRatio: 1,
-    width: 1280 * 2,
-    height: 720 * 2,
+    width: 1440,
+    height: 900,
     palette: getPalette(),
 
     // node only options
     asVideoFrames: false,
     filename: 'render',
-    outputDir: 'output'
-  };
-
-  function getPalette () {
-    var paletteColors = palettes[Math.floor(random() * palettes.length)];
-    return arrayShuffle(paletteColors);
+    outputDir: 'output',
   }
 
-  function arrayShuffle (arr) {
-    var rand;
-    var tmp;
-    var len = arr.length;
-    var ret = arr.slice();
+  function getPalette() {
+    var paletteColors = palettes[Math.floor(random() * palettes.length)]
+    return arrayShuffle(paletteColors)
+  }
+
+  function arrayShuffle(arr) {
+    var rand
+    var tmp
+    var len = arr.length
+    var ret = arr.slice()
 
     while (len) {
-      rand = Math.floor(random(1) * len--);
-      tmp = ret[len];
-      ret[len] = ret[rand];
-      ret[rand] = tmp;
+      rand = Math.floor(random(1) * len--)
+      tmp = ret[len]
+      ret[len] = ret[rand]
+      ret[rand] = tmp
     }
 
-    return ret;
+    return ret
   }
-};
+}
